0601a028385e62a040a4d95cf234620c80e320aa
Michael Matuzak <mmatuzak@gmail.com>
2016-12-29 11:08:41 -0800

    ch ch ch ch changes

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-var pressStart = require('press-start-font')
-var boot = require('./lib/boot')
-var mainMenu = require('./lib/main-menu')
-var gameLoop = require('./lib/game-loop')
-var gameOver = require('./lib/game-over')
+window.PIXI = require('phaser-ce/build/custom/pixi')
+window.p2 = require('phaser-ce/build/custom/p2')
+window.Phaser = require('phaser-ce/build/custom/phaser-split')
+
+const pressStart = require('press-start-font')
+const boot = require('./lib/boot')
+const mainMenu = require('./lib/main-menu')
+const gameLoop = require('./lib/game-loop')
+const gameOver = require('./lib/game-over')
 
 pressStart()
 
-var screenX = 398
-var screenY = 224
+const screenX = 398
+const screenY = 224
 
-var game = new Phaser.Game(screenX, screenY, Phaser.AUTO, '', null, false, false)
+const game = new Phaser.Game(screenX, screenY, Phaser.AUTO, '', null)
 
 game.state.add('boot', boot)
 game.state.add('mainMenu', mainMenu)
