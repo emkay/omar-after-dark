8b1899a24ae1dde8fd6c4f7e1062d7472047f426
Michael Matuzak <mmatuzak@gmail.com>
2017-01-07 15:35:10 -0800

    feature(tree): tree should look better now when you pass by it

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-window.PIXI = require('phaser-ce/build/custom/pixi')
-window.p2 = require('phaser-ce/build/custom/p2')
-window.Phaser = require('phaser-ce/build/custom/phaser-split')
-
+require('./phaser')
 const boot = require('./lib/boot')
 const mainMenu = require('./lib/main-menu')
 const gameLoop = require('./lib/game-loop')
