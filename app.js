ee8c8eaf3506b6df71c8ba3b5f440fa517456f73
Michael Matuzak <mmatuzak@gmail.com>
2017-01-07 22:23:30 -0800

    fix(blur): the whole damn thing was blury

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const gameOver = require('./lib/game-over')
 const screenX = 398
 const screenY = 224
 
-const game = new Phaser.Game(screenX, screenY, Phaser.AUTO, '', null)
+const game = new Phaser.Game(screenX, screenY, Phaser.CANVAS, '', null)
 
 game.state.add('boot', boot)
 game.state.add('mainMenu', mainMenu)
